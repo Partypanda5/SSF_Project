d809428e93f44d48b76427348ede16deab5d0f8a
partypanda5 <scoondi@hotmail.com>
2020-07-19 21:57:54 +0200

    Tablet view done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,12 @@
-// /* Write here your custom javascript codes */
+// /* Write here your custom javascript codes *//
+
+/* Open */
+function openNav() {
+    document.getElementById("responsiveNavbar").style.height = "100%";
+    console.log("hello");
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("responsiveNavbar").style.height = "0%";
+  }
