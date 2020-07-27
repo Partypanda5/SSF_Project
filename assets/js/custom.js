b3566106dbb6987d965d5d3e5a3e58c1d662103a
partypanda5 <scoondi@hotmail.com>
2020-07-27 21:31:13 +0200

    more mobile fixes from note pad

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,3 +10,33 @@ function openNav() {
   function closeNav() {
     document.getElementById("responsiveNavbar").style.height = "0%";
   }
+
+  function SwapImage(){
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+
+        h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+
+        Huntington1Cropped = document.getElementById('Huntington1Cropped');
+        Huntington3Cropped = document.getElementById('Huntington3Cropped');
+
+    // use conditionals to decide which image to show
+    if(w < 420 && h < 815)
+    {
+      Huntington1Cropped.style.backgroundImage = "url('assets/img/General/Huntington1Cropped.jpg')";
+
+      Huntington3Cropped.style.backgroundImage = "url('assets/img/General/Huntington3Cropped.jpg')";
+    }
+
+    if(w > 420 && h > 815)
+    {
+      Huntington1Cropped.style.backgroundImage = "url('assets/img/General/Huntington1.jpg')";
+
+      Huntington3Cropped.style.backgroundImage = "url('assets/img/General/Huntington3.jpg')";
+    }
+}
+
+SwapImage();
+
+$(window).resize(function() {
+  SwapImage();
+});
