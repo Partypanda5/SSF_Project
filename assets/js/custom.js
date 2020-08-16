00c4ee13a1bbca746bd782fab9705cfb13e2e882
partypanda5 <scoondi@hotmail.com>
2020-08-16 19:38:53 +0200

    fixed tablet view + added poppy notification + other adjustments

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,8 @@
 // /* Write here your custom javascript codes *//
 
+const acceptButton = document.querySelector("#accept");
+const notification = document.querySelector(".notification");
+
 /* Open */
 function openNav() {
     document.getElementById("responsiveNavbar").style.height = "100%";
@@ -11,4 +14,7 @@ function openNav() {
     document.getElementById("responsiveNavbar").style.height = "0%";
   }
 
-  
+  acceptButton.addEventListener("click", function()
+  {
+    notification.style.display = "none";
+  });
