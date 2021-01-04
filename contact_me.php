50ddb71734f73c70f3f82023983f300306c975b0
partypanda5 <scoondi@hotmail.com>
2021-01-04 11:44:35 +0200

    remvoed nat geo logo + contact form testing

--- contact_me.php
+++ contact_me.php
@@ -22,10 +22,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $subscribe = $_POST['subscribe'];
 
 // Create the email and send the message
-$to = 'info@sabisabifoundation.org.za'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'scoobdi@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Sabi Sabi Foundation Website Message From:  $name";
 $email_body = "You have received a new message from the Sabi Sabi Foundation website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n\n$message\n\nSubscribe to the newsletter:\n$subscribe";
-$headers = "From: noreply@SabiSabiFoundation.org\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@SabiSabiFoundation.org.za\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers) or die("An error has occurred. Please try again later.");
 echo "<h1 style='color: #28422C; text-align:center;  display: flex; justify-content: center; align-items: center; height: 200px;'> You message has been sent. Thank you for contacting us, we will get back to you soon.</h1>
